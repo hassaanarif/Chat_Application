8ef214f53c37dd4d9e87921353ae825d2a85613b
Hassaan Khawaja <hassaanarif.ha@gmail.com>
2022-07-06 15:52:23 +0500

    made some changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 
-import Join from "./components/Join";
-import Chat from "./components/Chat";
+import Join from "./components/Join/Join";
+import Chat from "./components/Chat/Chat";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/chat" element={<Chat />} />
         <Route path="/" element={<Join />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/Chat/Chat.jsx
@@ -0,0 +1,27 @@
+import queryString from "query-string";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import io from "socket.io-client";
+
+let socket;
+
+function Chat(props) {
+  let [name, setName] = useState("");
+  let [room, setRoom] = useState("");
+
+  let location = useLocation();
+  let data = queryString.parse(location.search);
+  const ENDPOINT = "localhost:5000";
+
+  useEffect(() => {
+    socket = io(ENDPOINT);
+
+    setName(data.name);
+    setRoom(data.room);
+
+    socket.emit("join", { name, room });
+  });
+  return <h1>Chat</h1>;
+}
+
+export default Chat;
